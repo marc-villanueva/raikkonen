172ac844a35ab38957cc5b2b5ef5c1b5a83e38b6
Marc Villanueva <marc.villanueva@activenetwork.com>
2013-11-07 07:34:56 -0800

    deserialize json to List<Event>

--- src/com/example/raikkonen/model/EventRepository.java
+++ src/com/example/raikkonen/model/EventRepository.java
@@ -1,6 +1,11 @@
 package com.example.raikkonen.model;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class EventRepository {
 
@@ -14,10 +19,16 @@ public class EventRepository {
 		try {
 			String location = Double.toString(latitude) + "," + Double.toString(longitude);
 			String json = client.searchEvents("", location);
-			return null;
+			return createListFromJson(json);
 		} catch (Exception e) {
 			// not sure how we want to handle REST exceptions
+			// delaying that decision for now
 			return null;
 		}
+	}
+
+	private List<Event> createListFromJson(String json) {
+		Type listType = new TypeToken<ArrayList<Event>>() {}.getType();
+        return new Gson().fromJson(json, listType);
 	}
 }
